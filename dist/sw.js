de56458984847f4c9954f9b44c0fc3ccd8951bc1
NextStepApp <Matt.l.wilson@att.net>
2025-09-11 22:31:49 -0500

    Add files via upload

--- dist/sw.js
+++ dist/sw.js
@@ -1,3 +1,5 @@
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (e) => e.waitUntil(self.clients.claim()));
 self.addEventListener("fetch", () => {});
+
+const CACHE = "next-step-cache-v2"; // was v1
